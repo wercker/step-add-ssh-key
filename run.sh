2dc77d6e43974bb07423023ead9a25bece4f183e
Dave Diamond <dave.diamond@oracle.com>
2018-03-08 12:19:19 -0500

    from feedback: use -z instead of len -eq 0; use dollar paren instead of back-tick for subshell; tidy up the detection of _PRIVATE

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ validate_key() {
   local private_key=$1;
 
   if [ -z "$private_key" ]; then
-    if [ "${#WERCKER_ADD_SSH_KEY_KEYNAME}" -eq 0 ] ; then
+    if [ -z "${WERCKER_ADD_SSH_KEY_KEYNAME}" ] ; then
       message="Private key not found. The keyname was blank, may indicate dollar sign prepended to the keyname."
     else
-      tmp=`echo "$WERCKER_ADD_SSH_KEY_KEYNAME" | sed -e 's/_PRIVATE$//'`
-      if [ "$tmp" != "$WERCKER_ADD_SSH_KEY_KEYNAME" ] ; then
+      tmp=$(echo "$WERCKER_ADD_SSH_KEY_KEYNAME" | sed -e 's/.*\(_PRIVATE\)$/\1/')
+      if [ "$tmp" = "_PRIVATE" ] ; then
         message="Private key not found. The keyname should not have _PRIVATE at the end."
       else
         message="Private key not found. Be sure to create an environment variable named ${WERCKER_ADD_SSH_KEY_KEYNAME}_PRIVATE (X_${WERCKER_ADD_SSH_KEY_KEYNAME}_PRIVATE if using the CLI) containing the SSH private key."
